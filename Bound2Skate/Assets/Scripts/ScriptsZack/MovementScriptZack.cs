0367a937940e1bd79453f1995a2e3355a995e332
HetingGeting <zack.bengtssonbystrom@elev.ga.klaragymnasium.se>
2024-12-11 14:03:49 +0100

    La till scripts
    Delade också in saker i filer

---
+++ Bound2Skate/Assets/Scripts/ScriptsZack/MovementScriptZack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptZack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
